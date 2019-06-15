eddff652d1f52e7e429f34ed9f9f21f6a8a5852e
gautambitious <mvgt.gautam@gmail.com>
2019-06-15 11:17:11 +0530

    commit 3

--- TickTackToe/app/src/main/java/app100/jain/com/ticktacktoe/game.kt
+++ TickTackToe/app/src/main/java/app100/jain/com/ticktacktoe/game.kt
@@ -2,8 +2,11 @@ package app100.jain.com.ticktacktoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class game : AppCompatActivity() {
+class game : AppCompatActivity(), View.OnClickListener{
+    override fun onClick(v: View?) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
