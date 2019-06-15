d14cb5203f7fce4ed267a0fc6964bc6e9bb72c53
gautambitious <mvgt.gautam@gmail.com>
2019-06-15 20:02:19 +0530

    Tick Tack Toe Logic Update 1

--- TickTackToe/app/src/main/java/app100/jain/com/ticktacktoe/MainActivity.kt
+++ TickTackToe/app/src/main/java/app100/jain/com/ticktacktoe/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 val Key1:String="b0os"
-val Key2:String="b0os"
+val Key2:String="b0os1"
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
     var i=Intent(this,game::class.java)
-       i.putExtra(Key1,textView.text.toString())
-        i.putExtra(Key2,textView1.text.toString())
+        if(textView.text.equals("") || textView1.text.equals("")){
+            Toast.makeText(this,"Player name can't be Empty",Toast.LENGTH_SHORT)
+        }
+        else{
+       i.putExtra(Key1,editText.text.toString())
+        i.putExtra(Key2,editText1.text.toString())
         startActivity(i)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- TickTackToe/app/src/main/java/app100/jain/com/ticktacktoe/game.kt
+++ TickTackToe/app/src/main/java/app100/jain/com/ticktacktoe/game.kt
@@ -1,15 +1,35 @@
 package app100.jain.com.ticktacktoe
 
+import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_game.*
 
 class game : AppCompatActivity(), View.OnClickListener{
+    var chance=true
+    val btn: Array<Button> = arrayOf(button,button1,button2,button3,button4,button5,button6,button7,button8)
     override fun onClick(v: View?) {
+        if(chance)
+            
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        textView1.text=intent.extras.getString(Key2)
+        textView.text=intent.extras.getString(Key1)
+        turn(chance)
+        for (i in btn.indices){
+            btn[i].setOnClickListener(this)
+        }
+    }
+    private fun turn(i: Boolean){
+        if(i)
+            textView.setBackgroundColor(Color.parseColor("#FFF5AA"))
+        else
+            textView1.setBackgroundColor(Color.parseColor("#FFF5AA"))
     }
 }
