bb8c7fa2e0e58661f2c66baf3179a279a2eb555b
gautambitious <mvgt.gautam@gmail.com>
2019-07-07 22:32:39 +0530

    Layout Add

--- Othello/app/src/main/java/app100/jain/com/othello/MainActivity.kt
+++ Othello/app/src/main/java/app100/jain/com/othello/MainActivity.kt
@@ -1,20 +1,41 @@
 package app100.jain.com.othello
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.view.marginBottom
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a=linearLayout{
-            for
+        linearLayout(){
+            orientation=LinearLayout.VERTICAL
+            layoutParams=ViewGroup.LayoutParams(matchParent, matchParent)
+        for(i in 0..7){
+            linearLayout(){
+                layoutParams=ViewGroup.LayoutParams(matchParent, wrapContent)
+                orientation=LinearLayout.HORIZONTAL
+                weightSum=8f
+                for (j in 0..7){
+                    button(){
+                        onClick {
+                            check()
+                        }
+                    }.lparams(width= wrapContent){
+                        padding=sp(0)
+                        weight=1f
+                        margin=sp(0)
+                    }
+                }
+            }
         }
-        }
-    }
+        }}
 
+    private fun check() {
+        toast("jhbjknk")
+            }
+}
