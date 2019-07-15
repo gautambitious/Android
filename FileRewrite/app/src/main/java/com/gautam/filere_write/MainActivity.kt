fd9834cc1a05a1ae1a56e28568ce24d871d975b5
gautambitious <mvgt.gautam@gmail.com>
2019-07-15 13:48:43 +0530

    readWriteFiles: text add package folder + retrive

--- FileRewrite/app/src/main/java/com/gautam/filere_write/MainActivity.kt
+++ FileRewrite/app/src/main/java/com/gautam/filere_write/MainActivity.kt
@@ -3,6 +3,8 @@ package com.gautam.filere_write
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("bro","Files dr ${filesDir.path}")
+        val file=File(filesDir,"myfile.text")
+        btn.setOnClickListener {
+//            file.writeText(editText.text.toString())
+            file.appendText(editText.text.toString())
+            editText.setText("")
+            Log.i("files","Exists= ${file.exists()}")
+        }
+
+        restoreBtn.setOnClickListener{
+            val existingFile=file.readText()
+            editText.setText(existingFile)
+        }
     }
 }
