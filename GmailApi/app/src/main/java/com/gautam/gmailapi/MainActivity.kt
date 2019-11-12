d196e48ad462ebd95df50a66ac0ce305db034bc6
gautambitious <mvgt.gautam@gmail.com>
2019-11-13 02:19:34 +0530

    Code Complete

--- GmailApi/app/src/main/java/com/gautam/gmailapi/MainActivity.kt
+++ GmailApi/app/src/main/java/com/gautam/gmailapi/MainActivity.kt
@@ -2,11 +2,30 @@ package com.gautam.gmailapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        send.setOnClickListener{
+
+                try {
+                    val sender =GMailSender("bennettdigitalcourierdesk@gmail.com", "niggerfuck")
+                    sender.sendMail("This is Subject",
+                        "This is Body",
+                        "bennettdigitalcourierdesk@gmail.com",
+                        "mvgt.gautam@gmail.com")
+                    Log.e("workk","damnn shit works")
+                } catch (e: Exception) {
+                    Log.e("workk", e.toString())
+                }
+        }
+
     }
 }
