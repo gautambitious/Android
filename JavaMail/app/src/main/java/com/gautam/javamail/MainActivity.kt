1bdaf2b95da47107349eacfba9914c78523e723c
gautambitious <mvgt.gautam@gmail.com>
2019-11-29 20:04:41 +0530

    JAVAMAIL Api

--- JavaMail/app/src/main/java/com/gautam/javamail/MainActivity.kt
+++ JavaMail/app/src/main/java/com/gautam/javamail/MainActivity.kt
@@ -2,12 +2,18 @@ package com.gautam.javamail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val jm=JavaMailAPI(this,"mvgt.gautam@gmail.com","check","kdnskndssadsds")
+            jm.execute()
+        }
 
     }
 }
