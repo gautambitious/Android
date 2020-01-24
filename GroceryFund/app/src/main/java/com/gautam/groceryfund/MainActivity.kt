ddba03fd77f48640ce637334356aaeb3dc42bf82
gautambitious <mvgt.gautam@gmail.com>
2020-01-24 15:06:12 +0530

    MAinACtivity updatte

--- GroceryFund/app/src/main/java/com/gautam/groceryfund/MainActivity.kt
+++ GroceryFund/app/src/main/java/com/gautam/groceryfund/MainActivity.kt
@@ -2,11 +2,19 @@ package com.gautam.groceryfund
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SwipeLayout.setOnRefreshListener {
+            textView.text = "Nigga gay"
+            SwipeLayout.isRefreshing = false
+        }
+
     }
 }
