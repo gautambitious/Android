40f559079b6125963b1464ce263a5fcc285f16a1
gautambitious <mvgt.gautam@gmail.com>
2020-01-27 14:18:12 +0530

    New Activity Add

---
+++ GroceryFund/app/src/main/java/com/gautam/groceryfund/UpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.gautam.groceryfund
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }
+}
